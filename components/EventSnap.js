61867b29ea189d294301439e914d47c1a00f5064
SmallBoy <zhalgasovaskhat@gmail.com>
2018-08-07 15:51:15 +0600

    beautiful2

--- components/EventSnap.js
+++ components/EventSnap.js
@@ -21,11 +21,18 @@ import {
   Toolbar,
   ToolbarAction,
   CardActions,
-  ToolbarContent
+  ToolbarContent,
+  CardCover
 } from "react-native-paper";
 import { Calendar, CalendarList, Agenda } from "react-native-calendars";
 import * as firebase from 'firebase'
 
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    fontSize: 18
+  }
+})
 
 export default class EventSnap extends React.Component {
   static navigationOptions = ({ navigation }) => {
@@ -83,41 +90,40 @@ export default class EventSnap extends React.Component {
     return (
       <View style={{ flex: 1 }}>
         <Card>
+          <CardCover
+            source = {{uri: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg'}}
+          />
           <CardContent>
             <Title style={{textAlign: 'center'}}>{event.title}</Title>
-            <Paragraph>{event.description}</Paragraph>
-            <Paragraph>Speaker {event.speaker}</Paragraph>
-            <Paragraph>Date: {event.date}</Paragraph>
-            <Paragraph>Time: {event.time}</Paragraph>
-            <Paragraph>Place: {event.place}</Paragraph>
-            <Paragraph>Available Seats: {event.seats - listOfUsers.length}</Paragraph>
+            <Paragraph style={styles.container}>{event.description}</Paragraph>
+            <Paragraph style={styles.container}>Speaker {event.speaker}</Paragraph>
+            <Paragraph style={styles.container}>Date: {event.date}</Paragraph>
+            <Paragraph style={styles.container}>Time: {event.time}</Paragraph>
+            <Paragraph style={styles.container}>Place: {event.place}</Paragraph>
+            <Paragraph style={styles.container}>Available Seats: {event.seats - listOfUsers.length}</Paragraph>
           </CardContent>
           {
             (auth_level == 'user') &&
-            (<CardActions>
+            (
               <Button raised primary onPress={() => {this.handleRegister(listOfUsers, event)}}>
                 Register
               </Button>
-            </CardActions>)
+            )
           }
           {
             (auth_level == 'guest') &&
             (
-              <CardActions>
                 <Button raised  primary onPress={() => this.props.navigation.push('Login')}>
                   Please sign in to register
                 </Button>
-              </CardActions>
             )
           }
           {
             (auth_level == 'admin') &&
             (
-              <CardActions>
               <Button>
                 Scan QR-codes
               </Button>
-            </CardActions>
             )
           }
       </Card>
